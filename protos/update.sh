1e69d84a58b1ac7a6f203da415bcbe17f91ec2df
Michael Hernandez <mhernan88@gmail.com>
2021-03-08 00:47:42 -0500

    added stock trade

--- protos/update.sh
+++ protos/update.sh
@@ -3,3 +3,4 @@ protoc --go_out=../go/schema/pb --go_opt=paths=source_relative --go-grpc_out=../
 protoc --go_out=../go/schema/pb --go_opt=paths=source_relative --go-grpc_out=../go/schema/pb --go-grpc_opt=paths=source_relative ./crypto_quote.proto
 protoc --go_out=../go/schema/pb --go_opt=paths=source_relative --go-grpc_out=../go/schema/pb --go-grpc_opt=paths=source_relative ./stock.proto
 protoc --go_out=../go/schema/pb --go_opt=paths=source_relative --go-grpc_out=../go/schema/pb --go-grpc_opt=paths=source_relative ./stock_quote.proto
+protoc --go_out=../go/schema/pb --go_opt=paths=source_relative --go-grpc_out=../go/schema/pb --go-grpc_opt=paths=source_relative ./stock_trade.proto
