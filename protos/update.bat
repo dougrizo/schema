ff6b6e249c4776cb420665aa0b4dda7749cb2a03
Michael Hernandez <mhernan88@gmail.com>
2021-03-08 23:01:45 -0500

    added crypto trade

--- protos/update.bat
+++ protos/update.bat
@@ -1,5 +1,6 @@
 protoc --go_out=../go/schema/pb --go_opt=paths=source_relative --go-grpc_out=../go/schema/pb --go-grpc_opt=paths=source_relative ./crypto.proto
 protoc --go_out=../go/schema/pb --go_opt=paths=source_relative --go-grpc_out=../go/schema/pb --go-grpc_opt=paths=source_relative ./crypto_quote.proto
+protoc --go_out=../go/schema/pb --go_opt=paths=source_relative --go-grpc_out=../go/schema/pb --go-grpc_opt=paths=source_relative ./crypto_trade.proto
 protoc --go_out=../go/schema/pb --go_opt=paths=source_relative --go-grpc_out=../go/schema/pb --go-grpc_opt=paths=source_relative ./stock.proto
 protoc --go_out=../go/schema/pb --go_opt=paths=source_relative --go-grpc_out=../go/schema/pb --go-grpc_opt=paths=source_relative ./stock_quote.proto
 protoc --go_out=../go/schema/pb --go_opt=paths=source_relative --go-grpc_out=../go/schema/pb --go-grpc_opt=paths=source_relative ./stock_trade.proto
--- protos/update.sh
+++ protos/update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 protoc --go_out=../go/schema/pb --go_opt=paths=source_relative --go-grpc_out=../go/schema/pb --go-grpc_opt=paths=source_relative ./crypto.proto
 protoc --go_out=../go/schema/pb --go_opt=paths=source_relative --go-grpc_out=../go/schema/pb --go-grpc_opt=paths=source_relative ./crypto_quote.proto
+protoc --go_out=../go/schema/pb --go_opt=paths=source_relative --go-grpc_out=../go/schema/pb --go-grpc_opt=paths=source_relative ./crypto_trade.proto
 protoc --go_out=../go/schema/pb --go_opt=paths=source_relative --go-grpc_out=../go/schema/pb --go-grpc_opt=paths=source_relative ./stock.proto
 protoc --go_out=../go/schema/pb --go_opt=paths=source_relative --go-grpc_out=../go/schema/pb --go-grpc_opt=paths=source_relative ./stock_quote.proto
 protoc --go_out=../go/schema/pb --go_opt=paths=source_relative --go-grpc_out=../go/schema/pb --go-grpc_opt=paths=source_relative ./stock_trade.proto
